af758f0d28e63ffb19d03088133e94e3426c6e72
badumbl <aia.vadim88@gmail.com>
2019-11-06 16:31:35 +0200

    #typo fix

--- src/main/java/com/badumbl/springsecurity/demo/controllers/DemoController.java
+++ src/main/java/com/badumbl/springsecurity/demo/controllers/DemoController.java
@@ -1,4 +1,4 @@
-package com.badumbl.springsecurity.demo.controlles;
+package com.badumbl.springsecurity.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
